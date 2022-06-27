181ac85b846a07297c207befd663f406c3d19cc6
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-06-27 13:51:10 +0300

    Seminar06 task10

--- seminar_06_10.java
+++ seminar_06_10.java
@@ -6,95 +6,94 @@ import java.util.Set;
 import java.util.Stack;
 
 public class seminar_06_10 {
-    static Map<String, Integer> map = new HashMap<>();
-    static Set<String> operations = new HashSet<>();
+  static Map<String, Integer> map = new HashMap<>();
+  static Set<String> operations = new HashSet<>();
 
-    static {
-        map.put("+", 1);
-        map.put("-", 1);
-        map.put("/", 2);
-        map.put("*", 2);
-        map.put("(", 0);
+  static {
+    map.put("+", 1);
+    map.put("-", 1);
+    map.put("/", 2);
+    map.put("*", 2);
+    map.put("(", 0);
 
-        operations.add("+");
-        operations.add("-");
-        operations.add("*");
-        operations.add("/");
+    operations.add("+");
+    operations.add("-");
+    operations.add("*");
+    operations.add("/");
+  }
 
-    }
+  public String postfixFrom(String infixExp) {
+    StringBuilder result = new StringBuilder();
+    Stack<String> stack = new Stack<>();
 
-    public String postfixFrom(String infixExp) {
-        StringBuilder result = new StringBuilder();
-        Stack<String> stack = new Stack<>();
-
-        String[] tokens = infixExp.split(" ");
-        for (String token : tokens) {
-            if (token.equals("(")) {
-                stack.push(token);
-            } else if (token.equals(")")) {
-                String removedToken = stack.pop();
-                while (!removedToken.equals("(")) {
-                    result.append(removedToken).append(" ");
-                    removedToken = stack.pop();
-                }
-            } else if (!operations.contains((token))) {
-                result.append(token).append(" ");
-                
-            } else {
-                while (!stack.isEmpty() && map.get(stack.peek()) >= map.get(token)) {
-                    stack.push(token);
-                }
-            }
-            while (!stack.isEmpty()) {
-                result.append(stack.pop()).append(" ");
-            }
-
+    String[] tokens = infixExp.split(" ");
+    for (String token : tokens) {
+      if (token.equals("(")) {
+        stack.push(token);
+      } else if (token.equals(")")) {
+        String removedToken = stack.pop();
+        while (!removedToken.equals("(")) {
+          result.append(removedToken).append(" ");
+          removedToken = stack.pop();
         }
-        return result.toString().trim();
-    }
 
-    public int evalRPN(String[] tokens) {
-        if (tokens.length == 1) {
-            return Integer.parseInt(tokens[0]);
+      } else if (!operations.contains((token))) {
+        result.append(token).append(" ");
+
+      } else {
+        while (!stack.isEmpty() && map.get(stack.peek()) >= map.get(token)) {
         }
-        int res = 0;
-        Stack<Integer> nums = new Stack<>();
-        Set<String> operations = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
-        for (String token : tokens) {
-            if (operations.contains(token)) {
-                int second = nums.pop();
-                int first = nums.pop();
-                nums.add(perform(token, first, second));
-                res = nums.peek();
-            } else {
-                nums.add(Integer.parseInt(token));
-            }
-
-        }
-        return res;
+        stack.push(token);
+      }
+    }
+    while (!stack.isEmpty()) {
+      result.append(stack.pop()).append(" ");
     }
 
-    int perform(String operator, int num1, int num2) {
-        switch (operator) {
-            case "+":
-                return num1 + num2;
-            case "-":
-                return num1 - num2;
-            case "*":
-                return num1 * num2;
-            case "/":
-                return num1 / num2;
-            default:
-                throw new RuntimeException();
-        }
+    return result.toString().trim();
+  }
+
+  public int evalRPN(String[] tokens) {
+    if (tokens.length == 1) {
+      return Integer.parseInt(tokens[0]);
     }
+    int result = 0;
+    Stack<Integer> nums = new Stack<>();
+    Set<String> operations = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+    for (String token : tokens) {
+      if (operations.contains(token)) {
+        int num2 = nums.pop();
+        int num1 = nums.pop();
 
-    public static void main(String[] args) {
-        seminar_06_10 solution = new seminar_06_10();
-        String res = solution.postfixFrom("2 + 5 - 33 * 7");
-        System.out.println(res);
-        System.out.println(solution.evalRPN(res.split(" ")));
-
+        nums.add(perform(token, num1, num2));
+        result = nums.peek();
+      } else {
+        nums.add(Integer.parseInt(token));
+      }
     }
+    return result;
+  }
 
+  int perform(String operations, int num1, int num2) {
+    switch (operations) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "*":
+        return num1 * num2;
+      case "/":
+        return num1 / num2;
+
+      default:
+        throw new RuntimeException();
+    }
+  }
+
+  public static void main(String[] args) {
+    seminar_06_10 solution = new seminar_06_10();
+    String res = solution.postfixFrom("2 + 5 - 33 * 7");
+    System.out.println(res);
+    System.out.println(solution.evalRPN(res.split(" ")));
+  }
 }
