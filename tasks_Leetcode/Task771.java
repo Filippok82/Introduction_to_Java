22ca1353b7987182f00219122964f03a8781fc35
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-06-30 10:53:10 +0300

    Solution task from leetcode

---
+++ tasks_Leetcode/Task771.java
@@ -0,0 +1,24 @@
+package tasks_Leetcode;
+
+
+
+class Task771 {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.contains("" + stones.charAt(i))) {
+                count++;
+            }
+            
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Task771 result=new Task771();
+        System.out.println(result.numJewelsInStones("aAb", "aAAbbbb"));;
+       
+
+       
+    }
+}
