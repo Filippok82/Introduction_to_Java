4261f54c306ae28ee34df62d1985991169047916
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-07-23 17:55:16 +0300

    Solution task1365 from leetcode

---
+++ tasks_Leetcode/Task1365.java
@@ -0,0 +1,26 @@
+package tasks_Leetcode;
+
+import java.util.Arrays;
+
+import javax.print.attribute.standard.NumberUpSupported;
+
+public class Task1365 {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j] && i != j) {
+                    count++;
+                }
+            }
+            res[i] = count;
+        }
+        return res;
+    }
+public static void main(String[] args) {
+    int[] nums = {8,1,2,2,3};
+    Task1365 result = new Task1365();
+    System.out.println(Arrays.toString(result.smallerNumbersThanCurrent(nums)));
+}
+}
