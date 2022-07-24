589e36318c551a75de22b0715d3a6b6066f0dd6b
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-07-24 10:05:33 +0300

    Solution task804 from leetcode

---
+++ tasks_Leetcode/Task804.java
@@ -0,0 +1,27 @@
+package tasks_Leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task804 {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morses = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+                ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+                "-.--", "--.." };
+        Set<String> uniqueMorses = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (Character c : word.toCharArray()) {
+                sb.append(morses['a' - c]);
+            }
+            uniqueMorses.add(sb.toString());
+        }
+        return uniqueMorses.size();
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "red", "get", "had" };
+        Task804 res = new Task804();
+        System.out.println(res.uniqueMorseRepresentations(words));
+    }
+}
