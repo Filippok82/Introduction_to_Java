bf06c183a6cbcd3469eb8eef0e45fd414cf5012d
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-08-08 11:57:06 +0300

    Solution task832 from leetcode

---
+++ tasks_Leetcode/Task832.java
@@ -0,0 +1,34 @@
+package tasks_Leetcode;
+
+import java.util.Arrays;
+
+public class Task832 {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n=image.length,m=image[0].length;
+        for(int i=0;i<n;i++)
+        {
+            int start=0,end=m-1;
+            while(start<=end)
+            {
+                int temp=image[i][start];
+                image[i][start]=image[i][end];
+                image[i][end]=temp;
+                if(start==end)
+                    image[i][start]^=1;
+                else{
+                image[i][start]^=1;
+                image[i][end]^=1;
+                }
+                start++;
+                end--;
+            }
+        }
+        return image;
+        
+    }
+public static void main(String[] args) {
+    int [] [] image = {{1,1,0},{1,0,1},{0,0,0}};
+    Task832 res =new Task832();
+    System.out.println(Arrays.deepToString(res.flipAndInvertImage(image)));
+}
+}
