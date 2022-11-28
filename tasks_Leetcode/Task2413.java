06cd4f5be90b94f4481fdd8d0c7caf6034528e1b
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-11-28 10:21:14 +0300

    Task2413

---
+++ tasks_Leetcode/Task2413.java
@@ -0,0 +1,13 @@
+package tasks_Leetcode;
+
+
+
+public class Task2413 {
+    public int smallestEvenMultiple(int n) {
+        return n % 2 == 0 ? n : n * 2;   
+    }
+public static void main(String[] args) {
+    Task2413 res = new Task2413();
+    System.out.println(res.smallestEvenMultiple(5));
+}
+}
